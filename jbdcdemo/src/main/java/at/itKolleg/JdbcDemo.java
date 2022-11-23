98cca31522b59caed2c25c66aacf30ae7def4080
Armin Hamzic <hamzo.93@hotmail.com>
2022-11-23 23:20:38 +0100

    remove wrong Dir

--- jbdcdemo/src/main/java/at/itKolleg/JdbcDemo.java
+++
@@ -1,9 +0,0 @@
-package at.itKolleg;
-
-public class JdbcDemo {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        //INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, 'Armin Hamzic', 'armin.ha@tsn.at'), (NULL, 'Hans Wurst', 'hans.99@tsn.at');
-    }
-}
